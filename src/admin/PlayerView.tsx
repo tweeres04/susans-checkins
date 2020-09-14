dc7858d59f51f0b950eef9191da07c143076b1b5
Tyler Weeres <tweeres04@gmail.com>
2020-09-13 18:45:08 -0700

    Use real data in the player view

--- src/admin/PlayerView.tsx
+++ src/admin/PlayerView.tsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import firebase from 'firebase/app';
+
+import {
+	useDocumentDataOnce,
+	useCollectionDataOnce,
+} from 'react-firebase-hooks/firestore';
+
+import { Player } from './player';
+import CheckinEntry from './CheckinEntry';
 
 import {
 	Card,
@@ -20,7 +29,10 @@ const testDates = generatePracticeDates();
 const testPlayer = generatePlayer();
 
 const testCheckins = testDates.map((d) =>
-	generateCheckinEntry({ player: testPlayer, timestamp: d })
+	generateCheckinEntry({
+		player: testPlayer,
+		timestamp: d as firebase.firestore.Timestamp,
+	})
 );
 
 const useStyles = makeStyles((theme) => ({
@@ -39,47 +51,72 @@ type PlayerViewProps = {
 export default function PlayerView({ playerId }: PlayerViewProps) {
 	const classes = useStyles();
 
-	const { name, dob, notes } = testPlayer;
+	const [
+		{ name, dob, notes, imageUrl } = {},
+		playerIsLoading,
+		playerError,
+	] = useDocumentDataOnce<Player>(
+		firebase.firestore().doc(`users/${playerId}`)
+	);
+
+	const [
+		checkins = [],
+		checkinsIsLoading,
+		checkinsError,
+	] = useCollectionDataOnce<CheckinEntry>(
+		firebase.firestore().collection(`users/${playerId}/checkins`),
+		{ idField: 'uid' }
+	);
+
+	const errors = [playerError, checkinsError].filter(Boolean);
 
 	return (
 		<>
-			<Card>
-				<CardContent>
-					<CardMedia
-						image={faker.image.people()}
-						className={classes.playerImage}
-					/>
-					<Typography variant="h3">{name}</Typography>
-					<Typography variant="h6">{dob}</Typography>
-					<Typography>{notes}</Typography>
-				</CardContent>
-			</Card>
-			<List>
-				{testCheckins.map(({ timestamp, checkin }) => {
-					const checkinSymptoms = hasSymptoms(checkin);
-					const symptomsPresent = checkinSymptoms.length > 0;
-					return (
-						<ListItem>
-							<ListItemText
-								primary={timestamp.toLocaleString()}
-								secondary={
-									<div>
-										{symptomsPresent
-											? `Symptoms: ${checkinSymptoms.join(', ')}`
-											: 'No symptoms'}
-									</div>
-								}
-								secondaryTypographyProps={{
-									component: 'div',
-									className: symptomsPresent
-										? classes.symptomsPresent
-										: undefined,
-								}}
-							/>
-						</ListItem>
-					);
-				})}
-			</List>
+			{errors.map((e) => (
+				<>
+					<p>{e.message}</p>
+					<p>{e.stack}</p>
+				</>
+			))}
+			{playerIsLoading || (
+				<Card>
+					<CardContent>
+						<CardMedia image={imageUrl} className={classes.playerImage} />
+						<Typography variant="h3">{name}</Typography>
+						<Typography variant="h6">{dob}</Typography>
+						<Typography>{notes}</Typography>
+					</CardContent>
+				</Card>
+			)}
+			{checkinsIsLoading || (
+				<List>
+					{checkins.map(({ timestamp, checkin }) => {
+						const checkinSymptoms = hasSymptoms(checkin);
+						const symptomsPresent = checkinSymptoms.length > 0;
+						const timestampDate = timestamp.toDate();
+						return (
+							<ListItem key={timestampDate}>
+								<ListItemText
+									primary={timestampDate.toLocaleString()}
+									secondary={
+										<div>
+											{symptomsPresent
+												? `Symptoms: ${checkinSymptoms.join(', ')}`
+												: 'No symptoms'}
+										</div>
+									}
+									secondaryTypographyProps={{
+										component: 'div',
+										className: symptomsPresent
+											? classes.symptomsPresent
+											: undefined,
+									}}
+								/>
+							</ListItem>
+						);
+					})}
+				</List>
+			)}
 		</>
 	);
 }
