6c7e61078384a47369295613c89ed0e220f5e7c5
Tyler Weeres <tweeres04@gmail.com>
2020-09-14 23:16:20 -0700

    Added nav drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { Suspense } from 'react';
-import { Router } from '@reach/router';
+import React, { useState, Suspense } from 'react';
+import { Router, Link } from '@reach/router';
 
 import firebase from 'firebase/app';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -11,7 +11,19 @@ import {
 	Toolbar,
 	Typography,
 	CircularProgress,
+	IconButton,
+	Drawer,
+	List,
+	ListItem,
+	ListItemIcon,
+	ListItemText,
 } from '@material-ui/core';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import PersonIcon from '@material-ui/icons/Person';
+
 import { makeStyles } from '@material-ui/core/styles';
 
 const CheckinForm = React.lazy(() => import('./CheckinForm/CheckinForm'));
@@ -25,7 +37,13 @@ const useStyles = makeStyles((theme) => ({
 	progress: {
 		margin: 'auto',
 		display: 'block',
-		marginTop: '10rem',
+		marginTop: theme.spacing(16),
+	},
+	drawerHeader: {
+		padding: theme.spacing(2),
+	},
+	drawerList: {
+		width: 200,
 	},
 }));
 
@@ -37,9 +55,14 @@ function Symptoms() {
 	return <p>Please stay home and get better.</p>;
 }
 
+const LinkWithRef = React.forwardRef((props, ref) => (
+	<Link ref={ref} {...props} />
+));
+
 export default function App() {
 	const classes = useStyles();
 	const [user, loading, error] = useAuthState(firebase.auth());
+	const [drawerOpen, setDrawerOpen] = useState(false);
 
 	return (
 		<Suspense
@@ -49,6 +72,15 @@ export default function App() {
 			<Container>
 				<AppBar>
 					<Toolbar>
+						<IconButton
+							color="inherit"
+							onClick={() => {
+								setDrawerOpen(true);
+							}}
+							edge="start"
+						>
+							<MenuIcon />
+						</IconButton>
 						<Typography component="h1" variant="h6">
 							Susans Checkins
 						</Typography>
@@ -73,6 +105,41 @@ export default function App() {
 					<Typography>There was an error signing in. Try again.</Typography>
 				)}
 			</Container>
+			<Drawer
+				open={drawerOpen}
+				onClose={() => {
+					setDrawerOpen(false);
+				}}
+			>
+				<Typography variant="h6" className={classes.drawerHeader}>
+					Susans Checkins
+				</Typography>
+				<List
+					className={classes.drawerList}
+					onClick={() => {
+						setDrawerOpen(false);
+					}}
+				>
+					<ListItem component={LinkWithRef} to="/" button>
+						<ListItemIcon>
+							<AssignmentTurnedInIcon />
+						</ListItemIcon>
+						<ListItemText primary="Checkin" />
+					</ListItem>
+					<ListItem component={LinkWithRef} to="/admin" button>
+						<ListItemIcon>
+							<VerifiedUserIcon />
+						</ListItemIcon>
+						<ListItemText primary="Admin" />
+					</ListItem>
+					<ListItem component={LinkWithRef} to="/profile" button>
+						<ListItemIcon>
+							<PersonIcon />
+						</ListItemIcon>
+						<ListItemText primary="Profile" />
+					</ListItem>
+				</List>
+			</Drawer>
 		</Suspense>
 	);
 }
